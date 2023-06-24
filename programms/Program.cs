ce020d0b978374d6698825d3e2c9ae35ea292d26
Maksim <norten1156@gmail.com>
2023-06-24 13:19:32 +0300

    решил вторую задачу

--- programms/Program.cs
+++ programms/Program.cs
@@ -1,7 +1,7 @@
 ﻿// 1)Задайте массив заполненный случайными положительными трехзначными числами.
 // Напишите программу, которая покажет количество четных чисел в массиве.
 // [345, 897, 568, 234] => 2
-internal class Program
+/*internal class Program
 {
     private static void Main(string[] args)
     {
@@ -37,6 +37,43 @@ internal class Program
             Console.WriteLine();
         }
     }
-}
+}*/
 //2 Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 //[345, 897, 568, 234] -> 2
+/*internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Console.WriteLine("Введите размер массива  ");
+        int size = Convert.ToInt32(Console.ReadLine());
+        int[] numbers = new int[size];
+        FillArrayRandomNumbers(numbers);
+        Console.WriteLine("массив: ");
+        PrintArray(numbers);
+        int sum = 0;
+
+        for (int z = 1; z < numbers.Length; z += 2)
+            sum = sum + numbers[z];
+
+        Console.WriteLine($"всего {numbers.Length} чисел, сумма элементов cтоящих на нечётных позициях = {sum}");
+
+        void FillArrayRandomNumbers(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = new Random().Next(1, 10);
+            }
+        }
+        void PrintArray(int[] numbers)
+        {
+            Console.Write("[ ");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+            Console.Write("]");
+            Console.WriteLine();
+        }
+    }
+}*/
